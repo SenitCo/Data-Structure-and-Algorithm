b2cc9b84ef9cf1a7caaf1bbb5b5ad77f26265e7b
SenitCo <hustershen@gmail.com>
2018-01-29 11:32:59 +0800

    oj

--- 28_matchString.cpp
+++ 28_matchString.cpp
@@ -37,7 +37,7 @@ public:
     }
 };
 
-/*****KMP算法*****/
+/*****KMP算法（这个实现版本很晦涩）*****/
 class Solution {
 public:
     int strStr(string haystack, string needle) {
@@ -77,6 +77,97 @@ private:
                 j = next[j - 1];
             else
                 next[i++] = 0;
+        }
+        return next;
+    }
+};
+
+/***************KMP算法*******************/
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int len1 = haystack.length(), len2 = needle.length();
+        if(len2 == 0)
+            return 0;
+        vector<int> next = getNext(needle);
+        int i = 0, j = 0;
+        while(i < len1 && j < len2)
+        {
+            if(j == -1 || haystack[i] == needle[j])
+            {
+                i++;
+                j++;
+            }
+            else
+                j = next[j];
+        }
+        if(j == len2)
+            return i - j;
+        return -1;
+    }
+private:
+    vector<int> getNext(string& needle)
+    {
+        int len = needle.length();
+        vector<int> next(len + 1);	//注意：next数组长度要大于子串长度
+        next[0] = -1;
+        for(int i = 0, j = -1; i < len;)
+        {
+            if(j == -1 || needle[i] == needle[j])
+            {
+                i++;	//i=len-1时，此处的自增可能导致下面next[i]溢出，因此next数组长度要大于子串长度
+                j++;
+                next[i] = j;
+            }
+            else 
+                j = next[j];
+        }
+        return next;
+    }
+};
+
+/***************KMP算法（改进版本）*******************/
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int len1 = haystack.length(), len2 = needle.length();
+        if(len2 == 0)
+            return 0;
+        vector<int> next = getNext(needle);
+        int i = 0, j = 0;
+        while(i < len1 && j < len2)
+        {
+            if(j == -1 || haystack[i] == needle[j])
+            {
+                i++;
+                j++;
+            }
+            else
+                j = next[j];
+        }
+        if(j == len2)
+            return i - j;
+        return -1;
+    }
+private:
+    vector<int> getNext(string& needle)
+    {
+        int len = needle.length();
+        vector<int> next(len + 1);
+        next[0] = -1;
+        for(int i = 0, j = -1; i < len;)
+        {
+            if(j == -1 || needle[i] == needle[j])
+            {
+                i++;
+                j++;
+                if(needle[i] != needle[j])
+                    next[i] = j;
+                else
+                    next[i] = next[j];
+            }
+            else 
+                j = next[j];
         }
         return next;
     }
