ff6c24c3ba47cf7c26432f4b8efd14c6cb92988e
SenitCo <hustershen@gmail.com>
2018-02-01 17:10:50 +0800

    oj

--- 144_preorderTraverseBiTree.cpp
+++ 144_preorderTraverseBiTree.cpp
@@ -9,6 +9,14 @@ For example: Given binary tree [1,null,2,3], return [1,2,3].
 Note: Recursive solution is trivial, could you do it iteratively?
 */
 
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
