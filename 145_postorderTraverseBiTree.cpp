6fa14bc45c5971148633b3d8b6f302d667b7c2e2
SenitCo <hustershen@gmail.com>
2018-02-01 16:44:29 +0800

    oj

---
+++ 145_postorderTraverseBiTree.cpp
@@ -0,0 +1,84 @@
+/*@Description: Binary Tree Postorder Traversal
+Given a binary tree, return the postorder traversal of its nodes' values.
+For example: Given binary tree [1,null,2,3], return [3,2,1].
+   1
+    \
+     2
+    /
+   3
+Note: Recursive solution is trivial, could you do it iteratively?
+*/
+
+/**
+递归法
+*/
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        traverse(root, result);
+        return result;
+    }
+private:
+    void traverse(TreeNode* node, vector<int>& result)
+    {
+        if(!node)    return;
+        traverse(node->left, result);
+        traverse(node->right, result);
+        result.push_back(node->val);
+    }
+};
+
+/**
+迭代法：后序遍历是左-右-根，将其转换为根-右-左（实现方法和先序遍历根-左-右类似，只是左右互换），
+最后将数组逆转即可
+*/
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> toVisit;
+        while(root != NULL || !toVisit.empty())
+        {
+            while(root)
+            {
+                result.push_back(root->val);
+                toVisit.push(root);
+                root = root->right;
+            }
+            root = toVisit.top();
+            toVisit.pop();
+            root = root->left;
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
+
+/**
+和上述迭代解法一致，只是将while循环替换成了if else
+*/
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> toVisit;
+        while(root != NULL || !toVisit.empty())
+        {
+            if(root)
+            {
+                result.push_back(root->val);
+                toVisit.push(root);
+                root = root->right;
+            }
+            else
+            {
+                root = toVisit.top();
+                toVisit.pop();
+                root = root->left;
+            }
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
---
+++ 147_insertionSortList.cpp
@@ -0,0 +1,72 @@
+/*@Description: Insertion Sort List
+Sort a linked list using insertion sort.
+*/
+
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+/**
+单链表的直接插入排序，每次将一个元素插入到排好序的序列中
+*/
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+            return head;
+        ListNode *start = new ListNode(0);
+        ListNode *cur = head, *pre = start, *next = NULL;
+        while(cur)
+        {
+            next = cur->next;
+            while(pre->next && pre->next->val < cur->val)
+                pre = pre->next;
+            cur->next = pre->next;
+            pre->next = cur;
+            pre = start;
+            cur = next;
+        }
+        return start->next;
+    }
+};
+
+/**
+单链表的选择排序，每次搜索无排序序列中的最小元素，将其加入到已排序序列的末尾，
+并使其下一个元素指向未排序序列
+*/
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+            return head;
+        ListNode *start = new ListNode(0);
+        start->next = head;
+        ListNode *cur, *node, *minNode, *minPre, *preNode;
+        for(cur = start; cur; cur = cur->next)
+        {
+            long long minVal = LLONG_MAX;
+            preNode = cur;
+            for(node = cur->next; node; node = node->next)
+            {
+				if(node->val < minVal)
+                {
+                    minVal = node->val;
+                    minNode = node;
+                    minPre = preNode;
+                }
+                preNode = node;
+            }
+            if(minPre != cur)
+            {    
+                ListNode *temp = cur->next;
+                cur->next = minNode;
+                minPre->next = minNode->next;
+                minNode->next = temp;
+            }
+        }
+        return start->next;
+    }
+};
