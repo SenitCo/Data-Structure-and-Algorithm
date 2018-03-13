7b1e5a16ff67772c75808c65d0bcb9020dbb9f03
SenitCo <hustershen@gmail.com>
2018-03-13 23:05:43 +0800

    .

--- 114_flattenBinaryTree.cpp
+++ 114_flattenBinaryTree.cpp
@@ -21,6 +21,10 @@ The flattened tree should look like:
              6
 */
 
+/**
+迭代法：对任意一个结点curr，其左子树（如果存在）按先序遍历的最后一个结点的右子结点为curr的右子结点，
+curr的右子结点为curr的左子结点
+*/
 class Solution {
 public:
     void flatten(TreeNode* root) {
@@ -31,12 +35,32 @@ public:
             {
                 TreeNode* prev = curr->left;
                 while(prev->right)
-                    prev = prev->right;
+                    prev = prev->right; //遍历到左子树的最后一个结点，用于连接curr的右子树
                 prev->right = curr->right;
-                curr->right = curr->left;
+                curr->right = curr->left;   //用当前结点的左子树替换右子树
                 curr->left = NULL;
             }
-            curr = curr->right;
+            curr = curr->right; //往右移动结点
         }
+    }
+};
+
+//递归法（先序遍历的逆转版本：右-左-中）
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode* prev = NULL;
+        reversePreorder(root, prev);
+    }
+private:
+    //prev指针必须得传引用，保持更新，或者定义成一个成员变量
+    void reversePreorder(TreeNode* node, TreeNode*& prev)   
+    {
+        if(node == NULL)    return;
+        reversePreorder(node->right, prev);
+        reversePreorder(node->left, prev);
+        node->right = prev;
+        node->left = NULL;
+        prev = node;
     }
 };
