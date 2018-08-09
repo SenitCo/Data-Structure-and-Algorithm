e1fbc4378d1b8365b429c1a0e4b66eb3a01b0a5b
SenitCo <hustershen@gmail.com>
2018-08-09 09:44:30 +0800

    oj

--- 042_trapRainWater.cpp
+++ 042_trapRainWater.cpp
@@ -1,18 +1,12 @@
 /*@Description: Trapping Rain Water. See https://github.com/SenitCo/Algorithm/blob/master/images/42_rainwatertrap.png
 Given n non-negative integers representing an elevation map where the width of each bar is 1, 
 compute how much water it is able to trap after raining.
-
-For example, 
-Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
-
-The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units 
-of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
+For example, Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
 */
 
 /**
 从左至右遍历，搜索可以储水的最大区域，并计算总和
 */
-
 class Solution {
 public:
     int trap(vector<int>& height) {
