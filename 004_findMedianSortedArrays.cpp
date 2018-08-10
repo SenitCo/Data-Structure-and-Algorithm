d8baf60ef8d370aa9f38880ae9d24bd510a24fc8
SenitCo <hustershen@gmail.com>
2018-08-10 22:19:52 +0800

    oj

--- 004_findMedianSortedArrays.cpp
+++ 004_findMedianSortedArrays.cpp
@@ -57,3 +57,39 @@ double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
         median = (nums[length / 2 - 1] + nums[length / 2]) / 2.0;
     return median;
 }
+
+/**
+log(min(m, n))解法 
+https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2481/Share-my-O(log(min(mn))-solution-with-explanation
+*/
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
+{
+    int m = nums1.size(), n = nums2.size();
+    if(m > n)
+        return findMedianSortedArrays(nums2, nums1);
+    int iMin = 0, iMax = m;
+    while(iMin <= iMax)
+    {
+        int i = (iMin + iMax) / 2;
+        int j = (m + n + 1) / 2 - i;
+        if(j != 0 && i != m && nums1[i] < nums2[j - 1])
+            iMin = i + 1;
+        else if(j != n && i != 0 && nums1[i - 1] > nums2[j])
+            iMax = i - 1;
+        else
+        {
+            int maxLeft = 0;
+            if(i == 0)  maxLeft = nums2[j - 1];
+            else if(j == 0) maxLeft = nums1[i - 1];
+            else    maxLeft = max(nums1[i - 1], nums2[j - 1]);
+            if((m + n) % 2 == 1)    return maxLeft;
+            
+            int minRight = 0;
+            if(i == m)  minRight = nums2[j];
+            else if(j == n) minRight = nums1[i];
+            else    minRight = min(nums1[i], nums2[j]);
+            return (maxLeft + minRight) / 2.0;
+        }
+    }
+    return 0.0;
+}
