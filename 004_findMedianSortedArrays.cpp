bab8c408b2cb43bf082c963a034dbe057394f58c
SenitCo <hustershen@gmail.com>
2018-08-10 22:53:34 +0800

    oj

--- 004_findMedianSortedArrays.cpp
+++ 004_findMedianSortedArrays.cpp
@@ -59,8 +59,9 @@ double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
 }
 
 /**
-log(min(m, n))解法 
+O(log(min(m, n)))解法 
 https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2481/Share-my-O(log(min(mn))-solution-with-explanation
+http://windliang.cc/2018/07/18/leetCode-4-Median-of-Two-Sorted-Arrays/
 */
 double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
 {
@@ -93,3 +94,32 @@ double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
     }
     return 0.0;
 }
+
+//O(log(m + n))解法 
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        int left = (n + m + 1) / 2, right = (n + m + 2) / 2;
+        return (getKth(nums1, 0, m - 1, nums2, 0, n - 1, left) + getKth(nums1, 0, m - 1, nums2, 0, n - 1, right)) * 0.5;
+    }
+
+    int getKth(vector<int>& nums1, int start1, int end1, vector<int>& nums2, int start2, int end2, int k)
+    {
+        int len1 = end1 - start1 + 1;
+        int len2 = end2 - start2 + 1;
+        if(len1 > len2)
+            return getKth(nums2, start2, end2, nums1, start1, end1, k);
+        if(len1 == 0)
+            return nums2[start2 + k - 1];
+        if(k == 1)
+            return min(nums1[start1], nums2[start2]);
+
+        int i = start1 + min(len1, k / 2) - 1;
+        int j = start2 + min(len2, k / 2) - 1;
+        if(nums1[i] > nums2[j])
+            return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+        else
+            return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
+    }
+};
